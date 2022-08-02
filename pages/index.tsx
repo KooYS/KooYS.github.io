033ce1219bc37406d6059d52805df0c0acec6c7f
KooYS <tlfkthsl42@gmail.com>
2022-08-02 18:01:35 +0900

    Chore: component extends field

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
 import { getAllPosts } from '@/libs/post';
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
+import { NextSeo } from 'next-seo';
 
 const Wrap = styled.div`
     width: 100%;
     height: 5000px;
     background-color: black;
 `;
-const Home: NextPage = ({ allPosts }: any) => {
+const Home: NextPage & { description?: string } = ({ allPosts }: any) => {
     console.log(allPosts);
-    return <Wrap></Wrap>;
+
+    return (
+        <Wrap>
+            <NextSeo title="Home" description="Home" />
+        </Wrap>
+    );
 };
 
+Home.displayName = 'Home';
+Home.description = 'HomeTie';
 export const getStaticProps = async () => {
     const allPosts = getAllPosts([
         'title',
