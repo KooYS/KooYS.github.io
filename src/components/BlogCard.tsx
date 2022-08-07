a3ef22f8e1daefa1f3a098de624846d345ff9209
koo <tlfkthsl42@gmail.com>
2022-08-07 23:11:19 +0900

    Feat: BlogCard

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,81 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { format } from 'date-fns';
+
+export interface Blog {
+    title: string;
+    date: string;
+    slug: string;
+    coverImage: string;
+    excerpt: string;
+}
+
+const Wrap = styled.a`
+    font-size: 14px;
+
+    &:hover {
+        background: var(--bg-color-0);
+    }
+`;
+const ImageWrap = styled.div`
+    width: 100%;
+    & img {
+        width: 100%;
+        height: 100%;
+        border-radius: 15px;
+        object-fit: cover;
+        object-position: center 10%;
+    }
+`;
+
+const ContentBody = styled.div`
+    padding: 10px;
+    & div {
+        padding: 4px 0;
+    }
+`;
+const StyledTitle = styled.div`
+    font-size: 2em;
+    white-space: unset;
+    position: relative;
+    top: 1px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+`;
+
+const StyledDate = styled.div``;
+
+const StyledShortDescription = styled.div`
+    line-height: 1.5;
+    white-space: unset;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+`;
+
+interface Props {
+    item: Blog;
+}
+const BlogCard: React.FC<Props> = ({ item }) => {
+    return (
+        <Wrap href={`/posts/${item.slug}`}>
+            <ImageWrap>
+                <img src={item.coverImage} alt={item.slug} />
+            </ImageWrap>
+            <ContentBody>
+                <StyledTitle>{item.title}</StyledTitle>
+                <StyledShortDescription>{item.excerpt}</StyledShortDescription>
+                <StyledDate>
+                    {format(new Date(item.date), 'yyyy-dd-MM')}
+                </StyledDate>
+            </ContentBody>
+        </Wrap>
+    );
+};
+
+export default BlogCard;
--- src/containers/BlogList.tsx
+++ src/containers/BlogList.tsx
@@ -1,13 +1,6 @@
 import styled from '@emotion/styled';
 import React from 'react';
-
-export interface Blog {
-    title: string;
-    date: string;
-    slug: string;
-    coverImage: string;
-    excerpt: string;
-}
+import BlogCard, { Blog } from '@/components/BlogCard';
 
 interface Props {
     list: Blog[];
@@ -24,32 +17,13 @@ const BlogListWrap = styled.div`
     column-gap: 20px;
     width: 100%;
 `;
-const BlogWrap = styled.a``;
-const ImageWrap = styled.div`
-    width: 100%;
-    & img {
-        width: 100%;
-        height: 100%;
-        border-radius: 15px;
-        object-fit: cover;
-        object-position: center 10%;
-    }
-`;
 
-const Body = styled.div``;
 const BlogList: React.FC<Props> = ({ list }) => {
     return (
         <Container>
             <BlogListWrap>
                 {list.map((el, index) => {
-                    return (
-                        <BlogWrap key={index}>
-                            <ImageWrap>
-                                <img src={el.coverImage} alt={el.slug} />
-                            </ImageWrap>
-                            <Body>{el.title}</Body>
-                        </BlogWrap>
-                    );
+                    return <BlogCard key={index} item={el} />;
                 })}
             </BlogListWrap>
         </Container>
