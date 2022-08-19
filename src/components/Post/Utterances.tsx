a94396bc2d8990dc98fbabff57b4ea683ca54c4a
KooYS <tlfkthsl42@gmail.com>
2022-08-19 14:10:47 +0900

    Fix: Utterances bug

--- src/components/Post/Utterances.tsx
+++ src/components/Post/Utterances.tsx
@@ -7,42 +7,72 @@ const StyledsSection = styled.section`
 `;
 const Utterances = () => {
     return (
-        <>
-            <StyledsSection
-                className="utteranc-light"
-                ref={(elem) => {
-                    if (!elem) {
-                        return;
-                    }
-                    elem.innerHTML = '';
-                    const scriptElem = document.createElement('script');
-                    scriptElem.src = 'https://utteranc.es/client.js';
-                    scriptElem.async = true;
-                    scriptElem.setAttribute('repo', 'KooYS/KooYS.github.io');
-                    scriptElem.setAttribute('issue-term', 'url');
-                    scriptElem.setAttribute('theme', 'github-light');
-                    scriptElem.crossOrigin = 'anonymous';
-                    elem.appendChild(scriptElem);
-                }}
-            />
-            <StyledsSection
-                className="utteranc-dark"
-                ref={(elem) => {
-                    if (!elem) {
-                        return;
-                    }
-                    elem.innerHTML = '';
-                    const scriptElem = document.createElement('script');
-                    scriptElem.src = 'https://utteranc.es/client.js';
-                    scriptElem.async = true;
-                    scriptElem.setAttribute('repo', 'KooYS/KooYS.github.io');
-                    scriptElem.setAttribute('issue-term', 'url');
-                    scriptElem.setAttribute('theme', 'github-dark');
-                    scriptElem.crossOrigin = 'anonymous';
-                    elem.appendChild(scriptElem);
-                }}
-            />
-        </>
+        <div>
+            <div className="utteranc-light">
+                <StyledsSection
+                    ref={(elem) => {
+                        if (!elem) {
+                            return;
+                        }
+                        let parent = elem.parentNode;
+                        if (elem.parentNode?.children) {
+                            for (
+                                let idx = 0;
+                                idx < elem.parentNode?.children.length;
+                                idx++
+                            ) {
+                                const element = elem.parentNode?.children[idx];
+                                elem.parentNode.removeChild(element);
+                            }
+                        }
+
+                        const scriptElem = document.createElement('script');
+                        scriptElem.src = 'https://utteranc.es/client.js';
+                        scriptElem.async = true;
+                        scriptElem.setAttribute(
+                            'repo',
+                            'KooYS/KooYS.github.io'
+                        );
+                        scriptElem.setAttribute('issue-term', 'url');
+                        scriptElem.setAttribute('theme', 'github-light');
+                        scriptElem.crossOrigin = 'anonymous';
+                        if (parent) parent.appendChild(scriptElem);
+                    }}
+                />
+            </div>
+
+            <div className="utteranc-dark">
+                <StyledsSection
+                    ref={(elem) => {
+                        if (!elem) {
+                            return;
+                        }
+
+                        let parent = elem.parentNode;
+                        if (elem.parentNode?.children)
+                            for (
+                                let idx = 0;
+                                idx < elem.parentNode?.children.length;
+                                idx++
+                            ) {
+                                const element = elem.parentNode?.children[idx];
+                                elem.parentNode.removeChild(element);
+                            }
+                        const scriptElem = document.createElement('script');
+                        scriptElem.src = 'https://utteranc.es/client.js';
+                        scriptElem.async = true;
+                        scriptElem.setAttribute(
+                            'repo',
+                            'KooYS/KooYS.github.io'
+                        );
+                        scriptElem.setAttribute('issue-term', 'url');
+                        scriptElem.setAttribute('theme', 'github-dark');
+                        scriptElem.crossOrigin = 'anonymous';
+                        if (parent) parent.appendChild(scriptElem);
+                    }}
+                />
+            </div>
+        </div>
     );
 };
 
