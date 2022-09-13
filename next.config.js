cb8ff73342e49e9ffa82c9e022d4a7cb7691e71e
KooYS <tlfkthsl42@gmail.com>
2022-09-13 12:35:58 +0900

    Chore: next version update(12.3)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    swcMinify: true,
     reactStrictMode: true,
     compiler: {
         emotion: true,
     },
-
     async rewrites() {
         return [
             {
